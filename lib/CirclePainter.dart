96975fd83ecc0d4c0b9d9b63e7b20e06c011a146
Javier Andres Pedrozo Payares <7519384+pedrozopayares@users.noreply.github.com>
2020-11-10 21:33:35 -0500

    Draw a circle at touch position x,y

--- lib/CirclePainter.dart
+++ lib/CirclePainter.dart
@@ -10,8 +10,6 @@ class CirclePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     double radius = 10.0;
-    double x = size.width / 2;
-    double y = size.height / 2;
     var paint = Paint();
     paint.color = Colors.yellowAccent;
     paint.strokeWidth = 5;
